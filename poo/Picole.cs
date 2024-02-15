6a3bd04dab15e0d079f3ea655616cdcfef8d800e
Nathália Helenna <148366887+F000XY@users.noreply.github.com>
2024-02-15 15:47:01 -0300

    Add files via upload

---
+++ poo/Picole.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo
+{
+    internal abstract class Picole
+    {
+        public abstract void sabor(string sabor);
+        public abstract void cor(string cor);
+        public abstract void tamanho(int tamanho);
+        public virtual void confeitos()
+        {
+            Console.WriteLine("o picole não possui confeitos");
+        }
+        public virtual void embalagem()
+        {
+            Console.WriteLine("embalagem transparente");
+        }
+
+        public virtual void preco() 
+        {
+           Console.WriteLine("O custo é de 5 reais. ");
+           
+        }
+
+    }
+
+    class PicoleDeChocolate : Picole
+    {
+        public override void sabor(string sabor)
+        { Console.WriteLine("O sabor do picole é : " +sabor); }
+        public override void cor(string cor)
+        { Console.WriteLine("A cor do picole é : " +cor); }
+
+        public override void tamanho(int tamanho)
+        {  Console.WriteLine("O tamanho do picole em cm é : " +tamanho); }
+        public override void preco()
+        {
+            Console.WriteLine("O custo é de 7 reais. ");
+
+        }
+
+    }
+
+    class PicoledeMorango : Picole
+    {
+        public PicoledeMorango()
+        { Console.WriteLine("Esse sovete vem com pedaços de morango!!!"); }
+        public override void sabor(string sabor)
+        { Console.WriteLine("O sabor do picole é : " + sabor); }
+        public override void cor(string cor)
+        { Console.WriteLine("A cor do picole é : " + cor); }
+
+        public override void tamanho(int tamanho)
+        { Console.WriteLine("O tamanho do picole em cm é : " + tamanho); }
+       
+    }
+
+    class PicoledeLimao : Picole
+    {
+        public PicoledeLimao()
+        { Console.WriteLine("Esse sovete é bem azedo viu!!!"); }
+        public override void sabor(string sabor)
+        { Console.WriteLine("O sabor do picole é : " + sabor); }
+        public override void cor(string cor)
+        { Console.WriteLine("A cor do picole é : " + cor); }
+
+        public override void tamanho(int tamanho)
+        { Console.WriteLine("O tamanho do picole em cm é : " + tamanho); }
+
+    }
+    class PicoleCorneto : Picole
+    {
+        public PicoleCorneto()
+        { Console.WriteLine("Esse sovete é em casquinha!!!"); }
+        public override void sabor(string sabor)
+        { Console.WriteLine("O sabor do picole é : " + sabor); }
+        public override void cor(string cor)
+        { Console.WriteLine("A cor do picole é : " + cor); }
+
+        public override void tamanho(int tamanho)
+        { Console.WriteLine("O tamanho do picole em cm é : " + tamanho); }
+
+        public override void confeitos()
+        {
+            Console.WriteLine("O sorvete de casquinha possui confeitos");
+        }
+        public override void embalagem()
+        {
+            Console.WriteLine("embalagem é de ilustrada e do no formato cônico");
+        }
+        public override void preco()
+        {
+            Console.WriteLine("O custo é de 10 reais. ");
+
+        }
+
+    }
+
+}
---
+++ poo/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saudacao.menu();
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("");
+
+            PicoleDeChocolate picole = new PicoleDeChocolate();
+            picole.tamanho(10);
+            picole.sabor("Chocolate");
+            picole.cor("Marrom");
+            picole.confeitos();
+            picole.embalagem();
+            picole.preco();
+            
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("");
+
+            PicoledeMorango picole2 = new PicoledeMorango();
+            picole2.tamanho(5);
+            picole2.sabor("Morrango");
+            picole2.cor("Rosa");
+            picole2.confeitos();
+            picole2.embalagem();
+            picole2.preco();
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("");
+
+            PicoledeLimao picole3 = new PicoledeLimao();
+            picole3.tamanho(5);
+            picole3.sabor("Limao");
+            picole3.cor("Branco");
+            picole3.confeitos();
+            picole3.embalagem();
+            picole3.preco();
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("");
+
+
+            PicoleCorneto picole4 = new PicoleCorneto();
+            picole4.tamanho(5);
+            picole4.sabor("Baunilha com Chocolate");
+            picole4.cor("Branco, com manchas escuras");
+            picole4.confeitos();
+            picole4.embalagem();
+            picole4.preco();
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("");
+
+            Saudacao.selecao();
+
+        }
+    }
+}
